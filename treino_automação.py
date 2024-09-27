56814e10046a59eb716bc93590b6f01d4f9e2995
Murilo Coelho <coelho.murilo10@gmail.com>
2024-09-26 23:31:20 -0300

    Add files via upload

---
+++ treino_automação.py
@@ -0,0 +1,94 @@
+import pyautogui
+import pyperclip
+import time
+import schedule
+
+
+
+
+
+
+pyautogui.hotkey("ctrl","t")
+                 
+time.sleep(3)
+
+   
+pyperclip.copy("https://web.whatsapp.com/")
+
+pyautogui.hotkey("ctrl", "v")
+ 
+pyautogui.hotkey("enter")
+
+   
+time.sleep(7)
+
+   
+
+   
+pyautogui.click(x=309, y=248)
+ 
+
+
+   
+time.sleep(2)
+
+   
+pyperclip.copy("Veia")
+
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.press("enter")
+
+
+
+time.sleep(2)
+
+
+ 
+
+texto1 = ('Já consigo enviar mensagens automaticas utilizando o Py ')
+   
+
+pyperclip.copy(texto1)
+
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.press("enter")
+
+
+
+time.sleep(3)
+
+ 
+
+pyautogui.click(x=309, y=248)
+ 
+   
+time.sleep(2)
+
+   
+pyperclip.copy("Veio")
+
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.press("enter")
+
+
+
+time.sleep(2)
+
+
+ 
+
+texto1 = ('Já consigo enviar mensagens automaticas utilizando o Py ')
+   
+
+pyperclip.copy(texto1)
+
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.press("enter")
+
+
+
+time.sleep(3)
